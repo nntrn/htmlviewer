f6ca82560536412f6d9929c0b4b84d9852cb6ffe
Annie_Tran <annie.tran@dell.com>
2022-02-23 02:15:58 +0000

    Resolve paths for windows

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
 const fs = require('fs')
 const path = require('path')
-const { getAllDirectoryFiles, createAndWrite, getUserHome, btoa } = require('./src/utils')
+const { getAllDirectoryFiles, createAndWrite, getUserHome } = require('./src/utils')
 const template = require('./src/template')
 
 const args = process.argv.slice(2)
 
 function readHTML(filepath, root) {
   const fileHtml = fs.readFileSync(filepath)
-  const doctitle = [ path.basename(filepath), fileHtml.toString().match(/<title>([^<]+)<\/title>/)].flat(2).filter(Boolean)
+  const doctitle = [path.basename(filepath), fileHtml.toString().match(/<title>([^<]+)<\/title>/)].flat(2).filter(Boolean)
   return {
     path: filepath,
     dirname: path.dirname(filepath).replace(root, '').split('/').filter(Boolean),
@@ -18,24 +18,42 @@ function readHTML(filepath, root) {
   }
 }
 
-const defaultOptions = (filepath = process.cwd()) => {
-  const rootFiles = getAllDirectoryFiles(filepath)
+function getNameFromPath(dirpath, maxFileNameLen = 140) {
+  const name = dirpath.split(path.sep).reverse().reduce((a, b, c) => {
+    if(a[0] + b.length > maxFileNameLen) {
+      return a
+    }
+    return [a[0] + b.length, [b.replace(/[^\d\w]+/g, ''), ...a[1]]]
+  }, [0, []])
+
+  return name[1].filter(Boolean).join('-')
+}
+
+function getDataHV(filepath = process.cwd(), userOptions = {}) {
+  const resolvedpath = path.resolve(filepath)
+  const rootFiles = getAllDirectoryFiles(resolvedpath)
 
   const htmlFiles = rootFiles
     .filter(e => /\.html$/.test(e.toLowerCase(), 'i'))
-    .map(e => readHTML(e, filepath))
+    .map(e => readHTML(e, resolvedpath))
 
   return {
     root: filepath,
-    dirpath: path.resolve(filepath),
+    dirpath: resolvedpath,
+    hvname: getNameFromPath(resolvedpath.replace(getUserHome(), '')),
     name: filepath.split('/').slice(-1)[0],
-    hash_path: btoa(path.resolve(filepath)).replace(/=/g, ''),
-    files: htmlFiles
+    sep: path.sep,
+    files: htmlFiles,
+    dom: {
+      toc: { id: 'hvtoc' },
+      iframe: { id: 'hvframe', name: 'hvframe' }
+    },
+    ...userOptions
   }
 }
 
-const data = defaultOptions(args[0])
-const htmloutputpath = `${getUserHome()}/.hv/${data.name}-${data.hash_path.slice(-10)}.html`
+const data = getDataHV(args[0])
+const htmloutputpath = [getUserHome(), '.hv', data.hvname + '.html'].join(path.sep)
 
 createAndWrite(htmloutputpath, template(data))
 
